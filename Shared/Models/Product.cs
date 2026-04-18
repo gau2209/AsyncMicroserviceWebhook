4be44541636790f3ce2f78f0d75e674a670a38ca
Gau2209 <anhtuanvo2209@gmail.com>
2026-04-18 23:33:24 +0700

    Add project files.

---
+++ Shared/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal price { get; set; }
+    }
+}
