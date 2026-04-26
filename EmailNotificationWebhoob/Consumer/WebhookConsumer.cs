b664f9e591497bc0ef6610f3583a45ab5db7e8f1
Gau2209 <anhtuanvo2209@gmail.com>
2026-04-26 13:07:39 +0700

    Create Webhook Consumer

---
+++ EmailNotificationWebhoob/Consumer/WebhookConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Shared.DTOs;
+
+namespace EmailNotificationWebhoob.Consumer
+{
+    public class WebhookConsumer : IConsumer<EmailDTOs>
+    {
+        private readonly HttpClient httpClient;
+
+        public WebhookConsumer (HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task Consume (ConsumeContext<EmailDTOs> context)
+        {
+            var result = await httpClient.PostAsJsonAsync("https://localhost:7083/email-webhook", new EmailDTOs(context.Message.Title,context.Message.Content));
+            result.EnsureSuccessStatusCode();
+        }
+    }
+}
